9c891fd2140e574d6a76d5a94d7dd171ae9ef8a4
neymarssj <143804583+neymarssj@users.noreply.github.com>
2024-05-17 15:26:42 -0300

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -109,7 +109,7 @@ let menu = `
 ⊜ *Pasatiempo(s)* ➺ ${user.pasatiempo === 0 ? '*Sin Registro*' : user.pasatiempo + '\n'}
 ⊜ *Experiencia ➟* ${exp} ⚡
 ⊜ *Diamantes ➟* ${limit} 💎
-⊜ *GataCoins ➟* ${money} 🐈
+⊜ *NeyCoins ➟* ${money} 🐈
 ⊜ *Tokens ➟* ${joincount} 🪙`.trim()
       
 const buttonParamsJson = JSON.stringify({
