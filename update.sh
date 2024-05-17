328af58637cb2eb3d88c8de4224e92ca42e346f2
neymarssj <143804583+neymarssj@users.noreply.github.com>
2024-05-17 14:53:15 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
   
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="GataBot-MD"  
-BOT_REPO="https://github.com/GataNina-Li/$BOT_DIR"
+BOT_REPO="https://github.com/neymarssj/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
